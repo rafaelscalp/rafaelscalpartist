8977f421c82ec89826248fdcfecdc4680858363e
Rafael Oropeza <rafaeloropeza@MacBook-Pro-de-Rafael.local>
2026-06-24 23:19:24 -0300

    Fix filtro de fecha

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,11 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob("blog/posts/*.md").reverse();
   });
 
+  eleventyConfig.addFilter("fechaCorta", function(date) {
+    const d = new Date(date);
+    return d.toLocaleDateString("es-AR", { day: "2-digit", month: "2-digit", year: "numeric" });
+  });
+
   return {
     dir: {
       output: "_site",
